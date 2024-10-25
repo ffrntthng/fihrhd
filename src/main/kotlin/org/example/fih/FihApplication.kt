288734fc3a904d60aab39ad229baa49f420d6f7f
ffrntthng <firuzamammadli04@gmail.com>
2024-10-25 13:19:42 +0400

    fateh

---
+++ src/main/kotlin/org/example/fih/FihApplication.kt
@@ -0,0 +1,11 @@
+package org.example.fih
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FihApplication
+
+fun main(args: Array<String>) {
+    runApplication<FihApplication>(*args)
+}
---
+++ src/test/kotlin/org/example/fih/FihApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.fih
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FihApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
